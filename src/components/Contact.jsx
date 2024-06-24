07f9f2c49ae3441ad956caa8022c4ee36fca03e7
egapay <etg5588@rit.edu>
2024-06-24 13:03:04 -0400

    Completed contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,4 @@
-import React, { Component } from 'react'
-import {
-  FormTextArea,
-  FormSelect,
-  FormRadio,
-  FormInput,
-  FormGroup,
-  FormCheckbox,
-  FormButton,
-  Form,
-} from 'semantic-ui-react'
-import 'semantic-ui-css/components/form.min.css'
+import { MdOutlineEmail } from "react-icons/md";
 
 import MainNavbar from "./MainNavbar";
 import SiteFooter from "./SiteFooter";
@@ -34,21 +23,42 @@ const Contact = () =>{
                 <FormTextArea label='About' placeholder='Tell us more about you...' />
                 <FormButton>Submit</FormButton>
             </Form> */}
-
-            <div className='flex justify-evenly h-fit shadow m-10'>
-                <div className='m-10 content-center'>
-                    <h1 className='text-5xl'>Contact Us!</h1>
+            <div className='flex lg:h-[70%] h-fit w-full lg:absolute content-center justify-center flex-wrap'>
+                <div className='content-center mt-8'>
+                    <h1 className='text-6xl lg:text-8xl'>Contact Us!</h1>
                 </div>
-                <div className='m-10 content-center'>
-                    <h1 className='text-3xl'>For all business inquiries:</h1>
-                    <div className='pt-11'>  
-                        <p>Email: </p>
-                        <p>Phone Number: </p>
+                <div className='flex justify-evenly h-fit shadow m-4 w-full pt-[3rem] pb-[8rem] sm:pb-[3rem] lg:py-[10rem] text-center flex-col lg:flex-row'>
+                    <div className='my-2 lg:mx-5'>
+                        <h1 className='text-4xl lg:text-5xl'>Bill Youngman</h1>
+                        <h2 className='text-2xl lg:text-2xl'>President</h2>
+                        <div className="items-center my-3" style={{display:"flex", justifyContent:"center"}}>
+                            <MdOutlineEmail size={25}/><a href="mailto:bill@margosdream.com" className="mx-1 text-lg">bill@margosdream.com</a>
+                        </div>
                     </div>
-                  
+                    <div className='my-2 lg:mx-5'>
+                        <h1 className='text-4xl lg:text-5xl'>Meghan Littlejohn</h1>
+                        <h2 className='text-2xl lg:text-2xl'>Vice President</h2>
+                        <div className="items-center my-3" style={{display:"flex", justifyContent:"center"}}>
+                            <MdOutlineEmail size={25}/><a href="mailto:meghan@margosdream.com" className="mx-1 text-lg">meghan@margosdream.com</a>
+                        </div>
+                    </div>
+                    <div className='my-2 lg:mx-5'>
+                        <h1 className='text-4xl lg:text-5xl'>Brooke Youngman</h1>
+                        <h2 className='text-2xl lg:text-2xl'>Vice President</h2>
+                        <div className="items-center my-3" style={{display:"flex", justifyContent:"center"}}>
+                            <MdOutlineEmail size={25}/><a href="mailto:brooke@margosdream.com" className="mx-1 text-lg">brooke@margosdream.com</a>
+                        </div>
+                    </div>
+                    <div className='my-2 lg:mx-5'>
+                        <h1 className='text-4xl lg:text-5xl'>Madison Youngman</h1>
+                        <h2 className='text-2xl lg:text-2xl'>Treasurer</h2>
+                        <div className="items-center my-3" style={{display:"flex", justifyContent:"center"}}>
+                            <MdOutlineEmail size={25}/><a href="mailto:madison@margosdream.com" className="mx-1 text-lg">madison@margosdream.com</a>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
-            
             <SiteFooter links={data.footer.links}/>
         </>
     )
